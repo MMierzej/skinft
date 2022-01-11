0d2812021392a63aac357ce586ac4f3e080b4849
Konrad Woźniak <darnokee@gmail.com>
2022-01-11 21:14:46 +0100

    added items page and repaired loading items on main page on open

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const ejs = require('ejs');
 const http = require('http');
 const multer = require('multer');
 const express = require('express');
+const req = require('express/lib/request');
 
 const app = express();
 let upload = multer();
@@ -15,14 +16,17 @@ app.use(express.static('./src/static'));
 
 let db = [];
 let image = fs.readFileSync('./images/test/2137.png', 'base64');
+// image = '';
 
 for (let i = 1; i <= 30; i++) {
     db.push({
         id: i,
         name: 'skin' + i,
         img: 'data:image/png;base64,' + image,
+        betterImg: 'data:image/png;base64,' + image, // tymczasowo
         priceUsd: 2,
-        description: 'Iprem Losum'
+        description: 'Flying fish few by the space station. She was the type of girl that always burnt sugar to show she cared. The hawk didnt understand why the ground squirrels didnt want to be his friend.',
+        state: 'available'
     })
 }
 
@@ -37,6 +41,10 @@ app.post('/items', upload.single(), (req, res) => {
     res.json(o);
 })
 
+app.get('/item/:id', (req, res) => {
+    let item = db[req.params.id]; // <-- Tutaj ladne zapytanie do bazy danych
+    res.render('item', {item});
+})
 
 http.createServer(app).listen(3000, () => {
     console.log('Server is running on port 3000...');
