e852a189582a78bb0beb2447ec289f09d3b1f2e6
Marcin Mierzejewski <322680@uwr.edu.pl>
2022-01-20 19:03:46 +0100

    Move the database to the cloud Tiny fixes to views

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,8 @@
+const fs = require('fs');
 const mongoose = require('mongoose');
-const dbUrl = 'mongodb://127.0.0.1:27017/test';
+// const dbUrl = 'mongodb://127.0.0.1:27017/test';
+const password = fs.readFileSync('./src/db/mongodb-password', 'utf-8');
+const dbUrl = `mongodb+srv://dbman:${password}@weppo-project.yolqd.mongodb.net/SKINFT?retryWrites=true&w=majority`
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
