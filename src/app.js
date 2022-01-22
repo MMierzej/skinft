90f7478d6b44aecf40ae7114dfc2c73055e496e8
Marcin Mierzejewski <322680@uwr.edu.pl>
2022-01-22 12:56:43 +0100

    Code organization

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ const fs = require('fs');
 const http = require('http');
 const multer = require('multer');
 const express = require('express');
-require('express/lib/request');
-const bodyParser = require('body-parser');
 const sessions = require('express-session');
 const cookieParser = require("cookie-parser");
 
@@ -23,13 +21,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(sessions({
     secret: "sekretnyKlucz2137222137", // to trzeba ukrywac
-    saveUninitialized:true,
+    saveUninitialized: true,
     cookie: { maxAge: 1000 * 60 * 60 * 24 * 10 }, // 10 dni
-    resave: false 
+    resave: false
 }));
 app.use(cookieParser());
-
-const urlencodedParser = bodyParser.urlencoded({ extended: false }); // necessary?
 
 
 app.get('/', upload.single(), (req, res) => {
@@ -49,50 +45,6 @@ app.get('/item/:name', async (req, res) => {
     const item = await Skin.findOne({ name: req.params.name });
     // console.log(item);
     res.render('item', { item });
-});
-
-app.get('/login', auth, (req, res) => {
-    if (req.logged) {
-        res.render('index'); // + jakis komunikat ze jestesmy zalogowani
-    }
-    res.render('login');
-});
-
-let sessionUser = {};  // sejse beda zapamietana w bazie danych chyba
-let sessionTime = {};
-
-app.post('/login', urlencodedParser, auth, (req, res) => {
-    if(req.logged) {
-        res.render('index'); // + jakis komunikat ze jestesmy zalogowani
-    }
-
-    let validUsername = 'adrian';
-    let validPassword = 'taxi';    
-
-    let username = req.body.username;
-    let password = req.body.password;
-
-    console.log(req.session);
-    // tutaj jakis request do bazy z walidacja
-
-    if(username === validUsername && password === validPassword) {  // tymczasowo
-        
-        let session=req.session;
-        session.userid = username;
-        console.log(req.session)
-        res.redirect('/');
-    }
-    else {
-        let message = {text: 'zly login lub haslo'};
-        res.render('login', {message});
-    }  
-});
-
-app.get('/register', auth, (req, res) => {
-    if (req.logged) {
-        res.render('index'); // + jakis komunikat ze jestesmy zalogowani
-    }
-    res.render('register');
 });
 
 app.get('/new-item', upload.single(), (req, res) => {
@@ -128,6 +80,46 @@ app.post('/new-item', upload.single(), async (req, res) => {
     res.redirect('/');
 });
 
+app.get('/login', auth, (req, res) => {
+    if (req.logged) {
+        res.render('index'); // + jakis komunikat ze jestesmy zalogowani
+    }
+    res.render('login');
+});
+
+app.post('/login', auth, (req, res) => {
+    if (req.logged) {
+        res.render('index'); // + jakis komunikat ze jestesmy zalogowani
+    }
+
+    let validUsername = 'adrian';
+    let validPassword = 'taxi';
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    console.log(req.session);
+    // tutaj jakis request do bazy z walidacja
+
+    if (username === validUsername && password === validPassword) {  // tymczasowo
+        let session = req.session;
+        session.userid = username;
+        console.log(req.session)
+        res.redirect('/');
+    }
+    else {
+        let message = { text: 'zly login lub haslo' };
+        res.render('login', { message });
+    }
+});
+
+app.get('/register', auth, (req, res) => {
+    if (req.logged) {
+        res.render('index'); // + jakis komunikat ze jestesmy zalogowani
+    }
+    res.render('register');
+});
+
 app.post('/register', upload.single(), auth, (req, res) => {
     if (req.logged) {
         res.render('index'); // + jakis komunikat ze jestesmy zalogowani
@@ -135,12 +127,12 @@ app.post('/register', upload.single(), auth, (req, res) => {
 
     // proces tworzenia konta, sprawdzamy czy email lub username nie sa zajete
     // jesli nie => dodajemy konto do bazy, pamietamy o szyfrowaniu hasla
-    
+
     res.render('login') // z komunikatem: konto zostalo utworzone
 });
 
 app.delete('/logout', auth, (req, res) => {
-    if(req.logged) {
+    if (req.logged) {
         req.session.destroy();
         // usuwamy sesje z bazy danych
     }
